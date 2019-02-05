e0ae0dd96903568ce1fd46755258930b7333aef6
Mateo <matheo2203@gmail.com>
2019-02-05 15:13:12 -0500

    Se implementa el patrón repository

---
+++ AppParking/Repository/Implementations/Mocks/TypeVehicleMock.swift
@@ -0,0 +1,16 @@
+//
+//  TypeVehicleMock.swift
+//  AppParking
+//
+//  Created by Mateo Gutiérrez Díaz - Ceiba Software on 2/5/19.
+//  Copyright © 2019 Mateo Gutiérrez Díaz - Ceiba Software. All rights reserved.
+//
+
+import Foundation
+
+class TypeVehicleMock: IRepositoryTypeVehicles {
+    
+    func getTypeVehicles(_ iAddVehicle: IAddVehicle) {
+        
+    }
+}
---
+++ AppParking/Repository/Implementations/Mocks/VehicleMock.swift
@@ -0,0 +1,24 @@
+//
+//  VehicleMock.swift
+//  AppParking
+//
+//  Created by Mateo Gutiérrez Díaz - Ceiba Software on 2/5/19.
+//  Copyright © 2019 Mateo Gutiérrez Díaz - Ceiba Software. All rights reserved.
+//
+
+import Foundation
+
+class VehicleMock: IRepositoryVehicle {
+    
+    func getVehicles(_ iListVehicles: IListVehicles) {
+        
+    }
+    
+    func addVehicle(_ iAddVehicle: IAddVehicle, _ vehicle: Vehicle) {
+        
+    }
+    
+    func deleteVehicle(_ iDeleteVehicle: IDeleteVehicle, _ plate: String) {
+        
+    }
+}
